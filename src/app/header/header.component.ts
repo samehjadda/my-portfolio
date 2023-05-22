26ca345e3c84ecb82103633d1c6cfee07618dbd7
samehjadda <samehjadda@gmail.com>
2023-05-22 16:05:06 -0400

    header, footer design changes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements AfterViewInit {
+
+  ngAfterViewInit() {
+    const hamburger = document.querySelector('.hamburger');
+    const nav = document.querySelector('#navbar');
+
+    if (hamburger && nav) {
+      hamburger.addEventListener('click', () => {
+        nav.classList.toggle('active');
+      });
+    }
+  }
 
 }
