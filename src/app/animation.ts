87894c835327956df55f29fc501eda815a647f49
samehjadda <samehjadda@gmail.com>
2023-05-23 17:54:11 -0400

    page designing

--- src/app/animation.ts
+++
@@ -1,9 +0,0 @@
-import { trigger, transition, style, animate } from '@angular/animations';
-
-export const fadeInAnimation =
-  trigger('fadeInAnimation', [
-    transition(':enter', [
-      style({ opacity: 0 }),
-      animate('3s', style({ opacity: 1 }))
-    ])
-  ]);
